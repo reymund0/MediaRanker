435d35391cde9c55192ee53a3f231f11ee99c4b8
Raymond Cook <raymondhowardcook@gmail.com>
2026-02-11 09:19:47 -0800

    Removed unnecessary comment

--- MediaRankerFrontend/src/lib/components/inputs/text-field/base-text-field.tsx
+++ MediaRankerFrontend/src/lib/components/inputs/text-field/base-text-field.tsx
@@ -1,15 +1,3 @@
-/*
-<TextField
-          fullWidth
-          label="Username or Email"
-          value={usernameOrEmail}
-          onChange={(e) => setUsernameOrEmail(e.target.value)}
-          required
-          margin="normal"
-          autoComplete="username"
-        />
-*/
-
 import { TextField } from "@mui/material";
 import { TextFieldProps } from "@mui/material";
 
